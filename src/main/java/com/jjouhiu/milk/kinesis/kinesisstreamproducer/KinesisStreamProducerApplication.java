a76b37e97b299e2680de1e11f84276ece54165bb
kilhankim <kilhan@a483e77a646e.ant.amazon.com>
2021-01-15 13:17:49 +0900

    first

---
+++ src/main/java/com/jjouhiu/milk/kinesis/kinesisstreamproducer/KinesisStreamProducerApplication.java
@@ -0,0 +1,13 @@
+package com.jjouhiu.milk.kinesis.kinesisstreamproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesisStreamProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinesisStreamProducerApplication.class, args);
+	}
+
+}
